6b4b89c9f2febcf3dd5a29c8dfd0a5f7c77aee5f
xziying <xziying@vip.qq.com>
2020-10-16 08:46:02 +0800

    优化权限对象算法

--- src/com/tjrac/studentAffairs/utils/CompPara.java
+++ src/com/tjrac/studentAffairs/utils/CompPara.java
@@ -21,12 +21,7 @@ public class CompPara {
      * @return  存在返回true 失败返回false
      */
     public boolean test(Integer comp) {
-        int purviewValue=(int)Math.pow(2,this.comp);
-        int value=purviewValue&(int)Math.pow(2,comp);
-        if(value==(int)Math.pow(2,comp)){
-            return true;
-        }
-        return false;
+        return (this.comp & comp) == comp;
     }
     /**
      * 添加权限
@@ -36,15 +31,11 @@ public class CompPara {
      */
     public boolean add(Integer comp) {
         //判断是否有comp权限
-        int purview =(int)Math.pow(2,this.comp);//没添加时的总权限
-        int userPurview=(int)Math.pow(2,comp)+purview;
-        int value=userPurview &((int)Math.pow(2,comp));//将新添加的权限值与之前的权限进行位运算用于判断是否重复
-        if(userPurview==value){
+        if(test(comp)){
             return false;
         }
-        //String number=Integer.toBinaryString(userPurview);//权限值为二进制
-         this.comp=userPurview;  //现在所具有的的权限值
-         return true;
+        this.comp += comp;
+        return true;
     }
     /**
      * 解析权限
@@ -52,19 +43,24 @@ public class CompPara {
      * 比如管理员权限1(001)和老师权限2(010)加起来是3(011)，二进制解析后，把不是0的位数解析回到list里，返回1和2的数组
      */
     public List<Integer> resolveCompetence() {
-        List<Integer> competence = new ArrayList<Integer>();
-        int i = 0;
-        int c=0;
-        int b =this.comp;
+        List<Integer> competence = new ArrayList<>();
+        int i = 1;
+        int b = this.comp;
         while (b != 0) {
-            c = b % 2;
-            if (c == 1) {
-                competence.add(i*2);
+            if (b % 2 == 1) {
+                competence.add(i);
             }
-            i++;
+            i*=2;
             b = b / 2;
         }
-
        return competence;
+    }
+
+    /**
+     * 返回权值值
+     * @return 权限值
+     */
+    public Integer getComp(){
+        return this.comp;
     }
 }
