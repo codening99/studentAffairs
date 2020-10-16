35d365729d97cb782d8deb4d66c090771291fa90
CodeNing <15076463800@163.com>
2020-10-16 21:06:28 +0800

    第一版用户(Student Teacher)服务层添加学生和老师信息

--- src/com/tjrac/studentAffairs/service/impl/UserServiceImpl.java
+++ src/com/tjrac/studentAffairs/service/impl/UserServiceImpl.java
@@ -4,10 +4,12 @@ import com.tjrac.studentAffairs.dao.BaseDao;
 import com.tjrac.studentAffairs.domain.user.Student;
 import com.tjrac.studentAffairs.domain.user.Teacher;
 import com.tjrac.studentAffairs.service.UserService;
+import com.tjrac.studentAffairs.utils.CompPara;
 import com.tjrac.studentAffairs.utils.JsonPack;
 import com.tjrac.studentAffairs.utils.MD5;
 
 import javax.servlet.http.HttpSession;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -171,16 +173,71 @@ public class UserServiceImpl implements UserService {
 
     /**
      * 增删查改 先检测是否拥有1(管理员权限)权限的账号进行
+     *
      * @param session 浏览器回话对象
-     * @param object   对象信息
-     * @return  返回Json字符串，参考README.md的文件的ajax请求接口
+     * @param object  对象信息
+     * @return 返回Json字符串，参考README.md的文件的ajax请求接口
      */
     @Override
     public String addObject(HttpSession session, Object object) {
 
-        
+        Object obj = session.getAttribute("user");
+        JsonPack json = new JsonPack();
 
-        return null;
+        int competence = -1;
+        //无论是学生还是老师，都获取他的权限
+        try {
+            Method getCompetence_id = obj.getClass().getDeclaredMethod("getCompetence_id");
+            competence = (int) getCompetence_id.invoke(obj); //获取到的权限大小
+        } catch (Exception e) {
+            System.out.println("获取权限失败");
+        }
+
+        if (competence == -1) {
+            json.put("event", 1);
+            json.put("msg", "获取权限失败");
+        } else {
+            //判断权限
+            CompPara compPara = new CompPara(competence);
+            if (compPara.test(1)) { //如果存在1权限
+
+                if (object instanceof Teacher) {  //要添加的为老师
+                    BaseDao baseDao = new BaseDao(Teacher.class);
+
+                    //判断数据库是否已经有同账户名的老师
+                    Object teacherDB = baseDao.query("account", ((Teacher) obj).getAccount());
+
+                    if (teacherDB != null) { //数据库中已存在该账户
+                        json.put("event", 2);
+                        json.put("msg", "账户已存在");
+                    } else {
+                        baseDao.insert(obj); //存入数据库
+                        json.put("event", 0);
+                        json.put("msg", "添加成功");
+                    }
+                } else {
+                    BaseDao baseDao = new BaseDao(Student.class);
+
+                    //判断数据库是否已经有同学号的学生
+                    Object teacherDB = baseDao.query("sno", ((Student) obj).getSno());
+
+                    if (teacherDB != null) { //数据库中已存在该账户
+                        json.put("event", 2);
+                        json.put("msg", "账户已存在");
+                    } else {
+                        baseDao.insert(obj); //存入数据库
+                        json.put("event", 0);
+                        json.put("msg", "添加成功");
+                    }
+                }
+
+            } else { //没有添加的权限
+                json.put("event", 1);
+                json.put("msg", "权限不足");
+            }
+        }
+
+        return json.toJson();
     }
 
     @Override
---
+++ src/com/tjrac/studentAffairs/test/TestMethod.java
@@ -0,0 +1,29 @@
+package com.tjrac.studentAffairs.test;
+
+import com.tjrac.studentAffairs.domain.user.Teacher;
+import org.junit.Test;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author ZeNing
+ * @create 2020-10-16 20:48
+ */
+public class TestMethod {
+
+    @Test
+    public void test() {
+
+        Teacher teacher = new Teacher();
+        teacher.setCompetence_id(1);
+
+        try {
+            Method getCompetence_id = Teacher.class.getDeclaredMethod("getCompetence_id");
+            System.out.println(getCompetence_id.invoke(teacher));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
