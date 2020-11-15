2d4f181ce29e0c59d2decc4391b0735775ca5b1c
CodeNing <15076463800@163.com>
2020-11-15 15:35:20 +0800

    年级 系别 专业 crud

--- src/com/tjrac/studentAffairs/service/user/ManagerService.java
+++ src/com/tjrac/studentAffairs/service/user/ManagerService.java
@@ -1,6 +1,8 @@
 package com.tjrac.studentAffairs.service.user;
 
+import com.tjrac.studentAffairs.domain.student.Department;
 import com.tjrac.studentAffairs.domain.student.Grade;
+import com.tjrac.studentAffairs.domain.student.Specialty;
 import com.tjrac.studentAffairs.domain.user.Teacher;
 
 import javax.servlet.http.HttpSession;
@@ -11,26 +13,64 @@ import javax.servlet.http.HttpSession;
  * @author : xziying
  * @create : 2020-10-23 18:14
  */
-public interface ManagerService extends TeacherService{
+public interface ManagerService extends TeacherService {
     /**
      * 增删查改老师账号信息 先检测是否拥有1(管理员权限)权限的账号进行
+     *
      * @param session 浏览器回话对象
-     * @param user   对象信息
-     * @return  返回Json字符串，参考README.md的文件的ajax请求接口
+     * @param user    对象信息
+     * @return 返回Json字符串，参考README.md的文件的ajax请求接口
      */
     String addUser(HttpSession session, Teacher user);
+
     String modifyUser(HttpSession session, Teacher user);
+
     String delUser(HttpSession session, Teacher user);
+
     String selectTeacher(HttpSession session);
 
     /**
      * 增删查改年级信息 先检测是否拥有1(管理员权限)权限的账号进行
+     *
      * @param session 浏览器回话对象
      * @param grade   年级信息
-     * @return  返回Json字符串，参考README.md的文件的ajax请求接口
+     * @return 返回Json字符串，参考README.md的文件的ajax请求接口
      */
     String addGrade(HttpSession session, Grade grade);
+
     String modifyGrade(HttpSession session, Grade grade);
+
     String delGrade(HttpSession session, Grade grade);
-    String selectGrade(HttpSession session, Grade grade);
+
+    String selectGrade(HttpSession session);
+
+    /**
+     * 增删查改系信息 先检测是否拥有1(管理员权限)权限的账号进行
+     *
+     * @param session    浏览器回话对象
+     * @param department 系信息
+     * @return 返回Json字符串，参考README.md的文件的ajax请求接口
+     */
+    String addDepartment(HttpSession session, Department department);
+
+    String modifyDepartment(HttpSession session, Department department);
+
+    String delDepartment(HttpSession session, Department department);
+
+    String selectDepartment(HttpSession session);
+
+    /**
+     * 增删查改专业信息 先检测是否拥有1(管理员权限)权限的账号进行
+     *
+     * @param session   浏览器回话对象
+     * @param specialty 专业信息
+     * @return 返回Json字符串，参考README.md的文件的ajax请求接口
+     */
+    String addSpecialty(HttpSession session, Specialty specialty);
+
+    String modifySpecialty(HttpSession session, Specialty specialty);
+
+    String delSpecialty(HttpSession session, Specialty specialty);
+
+    String selectSpecialty(HttpSession session);
 }
--- src/com/tjrac/studentAffairs/service/user/impl/ManagerServiceImpl.java
+++ src/com/tjrac/studentAffairs/service/user/impl/ManagerServiceImpl.java
@@ -1,7 +1,9 @@
 package com.tjrac.studentAffairs.service.user.impl;
 
 import com.tjrac.studentAffairs.dao.BaseDao;
+import com.tjrac.studentAffairs.domain.student.Department;
 import com.tjrac.studentAffairs.domain.student.Grade;
+import com.tjrac.studentAffairs.domain.student.Specialty;
 import com.tjrac.studentAffairs.domain.user.Teacher;
 import com.tjrac.studentAffairs.service.user.ManagerService;
 import com.tjrac.studentAffairs.utils.JsonPack;
@@ -19,7 +21,12 @@ import java.util.List;
 public class ManagerServiceImpl extends TeacherServiceImpl implements ManagerService {
     public final TypeInfo typeInfo = null;
     private final BaseDao<Grade> gradeDao = new BaseDao<>(Grade.class);
+    private final BaseDao<Department> departmentBaseDao = new BaseDao<>(Department.class);
+    private final BaseDao<Specialty> specialtyBaseDao = new BaseDao<>(Specialty.class);
 
+    /**
+     * 老师
+     */
     @Override
     public String addUser(HttpSession session, Teacher user) {
         JsonPack json = new JsonPack();
@@ -83,6 +90,9 @@ public class ManagerServiceImpl extends TeacherServiceImpl implements ManagerSer
         return json.toJson();
     }
 
+    /**
+     * 年级
+     */
     @Override
     public String addGrade(HttpSession session, Grade grade) {
         JsonPack json = new JsonPack();
@@ -102,24 +112,184 @@ public class ManagerServiceImpl extends TeacherServiceImpl implements ManagerSer
 
     @Override
     public String modifyGrade(HttpSession session, Grade grade) {
-        return null;
+        JsonPack json = new JsonPack();
+        int modify = gradeDao.modify(grade); //修改
+        if (modify == 404) {
+            json.put("event", 2);
+            json.put("msg", "没有该年级,无法修改");
+        } else if (modify == 0) {
+            json.put("event", 0);
+            json.put("msg", "修改成功");
+        } else {
+            json.put("event", 3);
+            json.put("msg", "修改失败");
+        }
+        return json.toJson();
     }
 
     @Override
     public String delGrade(HttpSession session, Grade grade) {
-        return null;
+        JsonPack json = new JsonPack();
+        int delete = gradeDao.delete(grade);//删除该账户信息
+        if (delete == 0) {
+            json.put("event", 0);
+            json.put("msg", "删除成功");
+        } else if (delete == 404){
+            json.put("event", 2);
+            json.put("msg", "没有找到年级信息，删除失败");
+        } else {
+            json.put("event", 3);
+            json.put("msg", "删除失败");
+        }
+        return json.toJson();
     }
 
     @Override
-    public String selectGrade(HttpSession session, Grade grade) {
-        return null;
+    public String selectGrade(HttpSession session) {
+        JsonPack json = new JsonPack();
+        List<Grade> grades = gradeDao.queryList();
+
+        json.put("event", 0);
+        json.put("count", grades.size());
+        json.put("grades", grades);
+        return json.toJson();
+    }
+
+    /**
+     * 系
+     */
+    @Override
+    public String addDepartment(HttpSession session, Department department) {
+        JsonPack json = new JsonPack();
+        int insert = departmentBaseDao.insert(department);
+        if (insert == 1062) {
+            json.put("event", 2);
+            json.put("msg", "该系已存在");
+        } else if (insert == 0) {
+            json.put("event", 0);
+            json.put("msg", "添加成功");
+        } else {
+            json.put("event", 3);
+            json.put("msg", "添加失败");
+        }
+        return json.toJson();
+    }
+
+    @Override
+    public String modifyDepartment(HttpSession session, Department department) {
+        JsonPack json = new JsonPack();
+        int modify = departmentBaseDao.modify(department); //修改
+        if (modify == 404) {
+            json.put("event", 2);
+            json.put("msg", "没有该系别,无法修改");
+        } else if (modify == 0) {
+            json.put("event", 0);
+            json.put("msg", "修改成功");
+        } else {
+            json.put("event", 3);
+            json.put("msg", "修改失败");
+        }
+        return json.toJson();
+    }
+
+    @Override
+    public String delDepartment(HttpSession session, Department department) {
+        JsonPack json = new JsonPack();
+        int delete = departmentBaseDao.delete(department);//删除该账户信息
+        if (delete == 0) {
+            json.put("event", 0);
+            json.put("msg", "删除成功");
+        } else if (delete == 404){
+            json.put("event", 2);
+            json.put("msg", "没有找到系别信息，删除失败");
+        } else {
+            json.put("event", 3);
+            json.put("msg", "删除失败");
+        }
+        return json.toJson();
+    }
+
+    @Override
+    public String selectDepartment(HttpSession session) {
+        JsonPack json = new JsonPack();
+        List<Department> departments = departmentBaseDao.queryList();
+
+        json.put("event", 0);
+        json.put("count", departments.size());
+        json.put("departments", departments);
+        return json.toJson();
+    }
+
+    /**
+     * 专业
+     */
+    @Override
+    public String addSpecialty(HttpSession session, Specialty specialty) {
+        JsonPack json = new JsonPack();
+        int insert = specialtyBaseDao.insert(specialty);
+        if (insert == 1062) {
+            json.put("event", 2);
+            json.put("msg", "该专业已存在");
+        } else if (insert == 0) {
+            json.put("event", 0);
+            json.put("msg", "添加成功");
+        } else {
+            json.put("event", 3);
+            json.put("msg", "添加失败");
+        }
+        return json.toJson();
+    }
+
+    @Override
+    public String modifySpecialty(HttpSession session, Specialty specialty) {
+        JsonPack json = new JsonPack();
+        int modify = specialtyBaseDao.modify(specialty); //修改
+        if (modify == 404) {
+            json.put("event", 2);
+            json.put("msg", "没有该专业,无法修改");
+        } else if (modify == 0) {
+            json.put("event", 0);
+            json.put("msg", "修改成功");
+        } else {
+            json.put("event", 3);
+            json.put("msg", "修改失败");
+        }
+        return json.toJson();
+    }
+
+    @Override
+    public String delSpecialty(HttpSession session, Specialty specialty) {
+        JsonPack json = new JsonPack();
+        int delete = specialtyBaseDao.delete(specialty);//删除该账户信息
+        if (delete == 0) {
+            json.put("event", 0);
+            json.put("msg", "删除成功");
+        } else if (delete == 404){
+            json.put("event", 2);
+            json.put("msg", "没有找到专业信息，删除失败");
+        } else {
+            json.put("event", 3);
+            json.put("msg", "删除失败");
+        }
+        return json.toJson();
+    }
+
+    @Override
+    public String selectSpecialty(HttpSession session) {
+        JsonPack json = new JsonPack();
+        List<Specialty> specialties = specialtyBaseDao.queryList();
+
+        json.put("event", 0);
+        json.put("count", specialties.size());
+        json.put("specialties", specialties);
+        return json.toJson();
     }
 
     @Test
     public void test() {
 //        System.out.println(addGrade(null, ));
         Grade grade = new Grade("grade");
-        System.out.println(gradeDao.insert(grade));
+        System.out.println(selectGrade(null));
     }
 
 }
